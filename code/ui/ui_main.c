dd4c21c42a3318c829b2ab595680e7b85362e3f0
Ilya Shabalin <lomtvador@gmail.com>
2023-07-19 18:49:32 +0300

    Fix include path for holocronicons.h

--- code/ui/ui_main.c
+++ code/ui/ui_main.c
@@ -18,7 +18,7 @@ USER INTERFACE MAIN
 #include "ui_force.h"
 #include "../cgame/animtable.h" //we want this to be compiled into the module because we access it in the shared module.
 #include "../game/bg_saga.h"
-#include "..\cgame\holocronicons.h"
+#include "../cgame/holocronicons.h"
 
 #include "mvsdk_setup.h"
 
