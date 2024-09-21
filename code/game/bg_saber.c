e5bed216d45e8899d77b5e2016b58a57ae7816e5
Tom <tomarrowtomarrow@hotmail.com>
2024-09-21 10:16:12 +0200

    Don't throw saber in racemode, for now.

--- code/game/bg_saber.c
+++ code/game/bg_saber.c
@@ -1571,7 +1571,8 @@ void PM_WeaponLightsaber(void)
 		pm->ps->saberCanThrow &&
 		pm->ps->fd.forcePower >= forcePowerNeeded[pm->ps->fd.forcePowerLevel[FP_SABERTHROW]][FP_SABERTHROW] &&
 		!BG_HasYsalamiri(pm->gametype, pm->ps) &&
-		BG_CanUseFPNow(pm->gametype, pm->ps, pm->cmd.serverTime, FP_SABERTHROW)
+		BG_CanUseFPNow(pm->gametype, pm->ps, pm->cmd.serverTime, FP_SABERTHROW) &&
+		!pm->ps->stats[STAT_RACEMODE] // don't throw sabers in racemode, its unpredictable what they will do, and they will affect our force power
 		)
 	{ //might as well just check for a saber throw right here
 		//This will get set to false again once the saber makes it back to its owner game-side
