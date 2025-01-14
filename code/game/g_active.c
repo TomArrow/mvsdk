3c2d33721471c89051a32dec8588dcd651f9f246
Tom <tomarrowtomarrow@hotmail.com>
2025-01-14 23:43:26 +0100

    Forgot to update one place of com_physicsfps warning?

--- code/game/g_active.c
+++ code/game/g_active.c
@@ -966,7 +966,13 @@ static qboolean ClientCheckNotifyPhysicsFps(gentity_t* ent) {
 			return qtrue; // We don't enforce anything on spectators
 		}
 		if (!client->pers.physicsFps.clientSetting) {
-			notification = "cp \"^2Toggle disabled.\n^7Please set com_physicsFps to a valid \nFPS setting you wish to play with\nor use ^2/togglefps ^7for toggle mode.\n\"";
+			//notification = "cp \"^2Toggle disabled.\n^7Please set com_physicsFps to a valid \nFPS setting you wish to play with\nor use ^2/togglefps ^7for toggle mode.\n\"";
+			if (client->pers.physicsFps.clientSendsPhysicsFps) {
+				notification = "cp \"^2Toggle disabled.\n^1Invalid ^7com_physicsFps value detected.\nYour movement may be restricted.\nPlease set a valid value\nor use ^2/togglefps ^7for toggle mode.\n\"";
+			}
+			else {
+				notification = "cp \"^2Toggle disabled.\n^1No ^7com_physicsFps value detected.\nYour movement may be restricted.\nPlease use a client with com_physicsFps\n^3(dev note: must be CVAR_USERINFO)\n^7or use ^2/togglefps ^7for toggle mode.\n\"";
+			}
 		}
 		else if (client->pers.physicsFps.clientSetting != client->pers.physicsFps.acceptedSetting) {
 			if (!client->pers.physicsFps.clientSettingValid) {
