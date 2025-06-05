c8ce1f535cc49ca33fa0fbb2edf08f8c69174951
Tom <tomarrowtomarrow@hotmail.com>
2025-06-05 02:20:57 +0200

    fix "to all servers" prefix messing up long names

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -3288,7 +3288,7 @@ void G_Say( gentity_t *ent, gentity_t *target, int mode, const char *chatText ) 
 	gentity_t	*other;
 	int			color;
 	char		name[64];
-	char		nameToAll[64];
+	char		nameToAll[93]; // 64+29
 	// don't let text be too long for malicious reasons
 	char		text[MAX_SAY_TEXT];
 	char		location[64];
