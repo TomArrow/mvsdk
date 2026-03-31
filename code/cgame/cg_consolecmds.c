85e77e8402573e3940e0cff3dc4ab568feae817d
Tom <tomarrowtomarrow@hotmail.com>
2026-03-31 08:12:28 +0200

    add ratemap for cmd completion

--- code/cgame/cg_consolecmds.c
+++ code/cgame/cg_consolecmds.c
@@ -2498,6 +2498,7 @@ void CG_InitConsoleCommands( void ) {
 	trap_AddCommand("say_cross");
 	trap_AddCommand("resseg");
 	trap_AddCommand("screenshotCubeMap");
+	trap_AddCommand("ratemap");
 
 	trap_AddCommand("tffaStats");
 }
